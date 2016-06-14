de85693b90f73796bee8cb44dba93fe9f5546e11
张钦 <492297036@qq.com>
2016-06-14 18:30:27 +0800

    提交

--- src/main/java/zhang/lao/console/ConsoleReqUrl.java
+++ src/main/java/zhang/lao/console/ConsoleReqUrl.java
@@ -6,4 +6,5 @@ package zhang.lao.console;
 public class ConsoleReqUrl {
 
     public static final String GOODS_INFO_EDIT = "/console/goods_info/edit";
+    public static final String GOODS_INFO_LIST_JSON = "/console/goods_info/json";
 }
--- src/main/java/zhang/lao/console/controller/GoodsController.java
+++ src/main/java/zhang/lao/console/controller/GoodsController.java
@@ -1,10 +1,14 @@
 package zhang.lao.console.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import zhang.lao.console.ConsoleReqUrl;
 import zhang.lao.mybatis.auto.dao.GoodsInfoMapper;
+import zhang.lao.pojo.resault.CommonResault;
 
 import javax.annotation.Resource;
 
@@ -22,5 +26,11 @@ public class GoodsController {
 
         modelMap.put("goods_info",goodsInfoMapper.selectByPrimaryKey(id));
         return "index";
+    }
+
+    @RequestMapping(value = ConsoleReqUrl.GOODS_INFO_LIST_JSON)
+    public @ResponseBody String json(int s,int p,ModelMap modelMap){
+        PageHelper.startPage(p,s);
+        return JSON.toJSONString(CommonResault.getSuccess(goodsInfoMapper.selectByExample(null)));
     }
 }
