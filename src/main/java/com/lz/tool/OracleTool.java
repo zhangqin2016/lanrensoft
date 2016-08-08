b5dcdac338a4ea83e92ca1f318cb97c10f9dd816
张钦 <492297036@qq.com>
2016-08-08 14:13:45 +0800

    添加oracle创建工具

---
+++ src/main/java/com/lz/tool/OracleTool.java
@@ -0,0 +1,51 @@
+package com.lz.tool;
+
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+/**
+ * Created by Administrator on 2016/7/23.
+ */
+@Component
+public class OracleTool {
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+    public String getNextStringNumber(String sequenceName) {
+        String sqls = "select count(*) from user_sequences where SEQUENCE_NAME='"+sequenceName.toUpperCase()+"' ";
+        if(jdbcTemplate.queryForObject(sqls,BigDecimal.class).intValue()==0){
+            String sqlSeq = "create sequence " + sequenceName + " increment by 1 start with 1 NOMAXVALUE NOCYCLE CACHE 10";
+            jdbcTemplate.update(sqlSeq);
+        }else{
+            //DO NOTHING
+        }
+        String sql = "select " + sequenceName + ".nextVal from dual";
+        return String.valueOf(jdbcTemplate.queryForObject(sql,BigDecimal.class));
+    }
+    public Long getNextLongNumber(String sequenceName) {
+        String sqls = "select count(*) from user_sequences where SEQUENCE_NAME='"+sequenceName.toUpperCase()+"' ";
+        if(jdbcTemplate.queryForObject(sqls,BigDecimal.class).intValue()==0){
+            String sqlSeq = "create sequence " + sequenceName + " increment by 1 start with 1 NOMAXVALUE NOCYCLE CACHE 10";
+            jdbcTemplate.execute(sqlSeq);
+        }else{
+            //DO NOTHING
+        }
+        String sql = "select " + sequenceName + ".nextVal from dual";
+        return jdbcTemplate.queryForObject(sql,BigDecimal.class).longValue();
+    }
+    public BigDecimal getNextNumber(String sequenceName) {
+        String sqls = "select count(*) from user_sequences where SEQUENCE_NAME='"+sequenceName.toUpperCase()+"' ";
+        if(jdbcTemplate.queryForObject(sqls,BigDecimal.class).intValue()==0){
+            String sqlSeq = "create sequence " + sequenceName + " increment by 1 start with 1 NOMAXVALUE NOCYCLE CACHE 10";
+            jdbcTemplate.update(sqlSeq);
+        }else{
+            //DO NOTHING
+        }
+        String sql = "select " + sequenceName + ".nextVal from dual";
+        return jdbcTemplate.queryForObject(sql,BigDecimal.class);
+    }
+}
