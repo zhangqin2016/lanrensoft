4de3eceb185a3a33dfe6aa30d0c89b9b0da2fb96
张钦 <492297036@qq.com>
2016-08-11 11:13:53 +0800

    初始化提交

--- src/main/java/zhang/lao/console/model/login/LoginUserModel.java
+++ src/main/java/zhang/lao/console/model/login/LoginUserModel.java
@@ -42,6 +42,7 @@ public static final String loginTypeApp="2";
 	private String phone;
 	private Integer verify_email;
 	private String user_pic;
+	private String userNickName;
 	private Integer status;
 	private Integer da_id;
 	private Integer create_user_id;
@@ -118,5 +119,20 @@ public static final String loginTypeApp="2";
 	public void setCreate_time(Date create_time) {
 		this.create_time = create_time;
 	}
-	
+
+	public String getUserNickName() {
+		return userNickName;
+	}
+
+	public void setUserNickName(String userNickName) {
+		this.userNickName = userNickName;
+	}
+
+	public static String getLoginTypeSys() {
+		return loginTypeSys;
+	}
+
+	public static String getLoginTypeApp() {
+		return loginTypeApp;
+	}
 }
