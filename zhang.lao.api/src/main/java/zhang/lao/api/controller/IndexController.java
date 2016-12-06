de52102d0e90ebf5e205fd02748663ade5eff68f
492297036@qq.com <zhangqin0000q>
2016-12-06 15:19:09 +0800

    将项目分层

--- zhang.lao.api/src/main/java/zhang/lao/api/controller/IndexController.java
+++ zhang.lao.api/src/main/java/zhang/lao/api/controller/IndexController.java
@@ -2,6 +2,7 @@ package zhang.lao.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import zhang.lao.pojo.resp.api.ApiRespData;
 
 /**
@@ -12,7 +13,7 @@ import zhang.lao.pojo.resp.api.ApiRespData;
 public class IndexController {
 
     @RequestMapping("/")
-    public ApiRespData index(){
+    public @ResponseBody ApiRespData index(){
         return ApiRespData.buildSucc();
     }
 }
