0a481f8ae413e23970fd3e420a0c5337427d6f51
zhangqin <zhangq@yhyvr.com>
2017-03-23 19:04:19 +0800

    springboot上传

---
+++ zhang-lao-bbs/src/main/java/lz/bbs/controller/IndexController.java
@@ -0,0 +1,11 @@
+package lz.bbs.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhangqin on 2017/3/22.
+ */
+
+@RestController
+public class IndexController {
+}
