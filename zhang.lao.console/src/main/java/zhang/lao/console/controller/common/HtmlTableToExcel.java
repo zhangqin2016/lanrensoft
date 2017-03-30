b962db9ad791aa9cb855f11ab17e9100743f8911
zhangqin <zhangq@yhyvr.com>
2017-03-30 18:59:36 +0800

    添加excel导出

---
+++ zhang.lao.console/src/main/java/zhang/lao/console/controller/common/HtmlTableToExcel.java
@@ -0,0 +1,46 @@
+package zhang.lao.console.controller.common;
+
+import com.lz.kit.LogKit;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+/**
+ * Created by zhangqin on 2017/3/30.
+ */
+@Controller
+public class HtmlTableToExcel {
+
+    @RequestMapping("/console/export/excel")
+    public void exportExcel(String table, HttpServletResponse response){
+       String  fileName = System.currentTimeMillis()+".xls";
+            response.reset();
+            response.setHeader("Content-Disposition", String.format("attachment; filename=%s", fileName));
+            response.setHeader("Content-Type","application/msexcel");
+            response.setContentType("application/vnd.ms-excel;charset=utf-8");
+            response.addCookie(new Cookie("fileDownload", "true"));
+            //out.write(new byte[]{(byte) 0xEF, (byte) 0xBB, (byte) 0xBF}); // 生成带bom的utf8文件
+        OutputStream out = null;
+        try {
+            out = response.getOutputStream();
+            out.write(table.getBytes());
+            out.flush();
+        } catch (IOException e) {
+            LogKit.error(e.getMessage(),e);
+        }finally {
+            if(out!=null){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    LogKit.error(e.getMessage(),e);
+                }
+            }
+        }
+
+    }
+}
