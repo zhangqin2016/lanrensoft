f4ac64cbf3372e01dcd51b6075791b34bc135a5d
zhangqin <zhangq@yhyvr.com>
2017-08-22 16:32:40 +0800

    优化

---
+++ zhang.lao.dao/src/main/java/zhang/lao/dao/base/SysReqUrlGroupDao.java
@@ -0,0 +1,15 @@
+package zhang.lao.dao.base;
+
+import zhang.lao.build.mybatis.jdbc.auto.dao.SysReqUrlGroupMapper;
+import zhang.lao.build.mybatis.jdbc.auto.model.SysReqUrlGroup;
+
+import java.util.Map;
+
+/**
+* 
+* @author 
+* @create 
+**/
+public interface SysReqUrlGroupDao extends SysReqUrlGroupMapper {
+
+}
