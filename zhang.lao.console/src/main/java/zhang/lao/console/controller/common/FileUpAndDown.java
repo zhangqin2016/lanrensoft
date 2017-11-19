73e266e6bfd5c189019d37a35846c2632b59ae56
张钦 <492297036@qq.com>
2017-11-19 11:36:30 +0800

    优化

--- zhang.lao.console/src/main/java/zhang/lao/console/controller/common/FileUpAndDown.java
+++ zhang.lao.console/src/main/java/zhang/lao/console/controller/common/FileUpAndDown.java
@@ -20,8 +20,8 @@ import zhang.lao.build.tool.date.DateUtil;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Date;
 import java.util.Iterator;
 
@@ -77,4 +77,47 @@ public class FileUpAndDown{
 		return HttpResultUtil.buildError(HttpResultEnum.UPLOADERROR);
 	}
 
+	
+	@RequestMapping("/file/upload/ckeditor")
+	public void uploadCkeditor(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+		if(multipartResolver.isMultipart(request)) {
+			//转换成多部分request
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			Iterator<String> fileNames = multipartRequest.getFileNames();
+			String fileName = fileNames.next();
+			MultipartFile uploadFile = multipartRequest.getFile(fileName);
+			PrintWriter out =  response.getWriter();
+			// CKEditor提交的很重要的一个参数    
+			String callback = request.getParameter("CKEditorFuncNum");
+			String expandedName = "";  //文件扩展名    
+			String uploadContentType = uploadFile.getContentType();
+			if (uploadContentType.equals("image/pjpeg") || uploadContentType.equals("image/jpeg")) {
+				//IE6上传jpg图片的headimageContentType是image/pjpeg，而IE9以及火狐上传的jpg图片是image/jpeg    
+				expandedName = ".jpg";
+			}else if(uploadContentType.equals("image/png") || uploadContentType.equals("image/x-png")){
+				//IE6上传的png图片的headimageContentType是"image/x-png"    
+				expandedName = ".png";
+			}else if(uploadContentType.equals("image/gif")){
+				expandedName = ".gif";
+			}else if(uploadContentType.equals("image/bmp")){
+				expandedName = ".bmp";
+			}else{
+				out.println("<script type=\"text/javascript\">");
+				out.println("window.parent.CKEDITOR.tools.callFunction(" + callback + ",''," + "'文件格式不正确（必须为.jpg/.gif/.bmp/.png文件）');");
+				out.println("</script>");
+			}
+			if(request.getContentLength() > 1024*1024){
+				out.println("<script type=\"text/javascript\">");
+				out.println("window.parent.CKEDITOR.tools.callFunction(" + callback + ",''," + "'文件大小不得大于600k');");
+				out.println("</script>");
+			}
+
+			SysFile sysFile = uploadService.uploadToAli(uploadFile);
+			// 返回"图像"选项卡并显示图片    
+			out.println("<script type=\"text/javascript\">");
+			out.println("window.parent.CKEDITOR.tools.callFunction(" + callback + ",'" + "" + sysFile.getFileUrl() + "','')");
+			out.println("</script>");
+		}
+	}
 }
