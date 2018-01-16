f9feb3de4761d49b61ad005eed1177a0c0c5333d
zhangqin <492297036@qq.com>
2018-01-16 15:12:14 +0800

    修改工具类

--- zhang.lao.build/src/main/java/zhang/lao/build/tool/ListUtils.java
+++ zhang.lao.build/src/main/java/zhang/lao/build/tool/ListUtils.java
@@ -11,4 +11,23 @@ public class ListUtils {
 
         return list==null||list.size()==0?null:  list.get(0);
     }
+    /**
+     * 判断是否为空
+     * @param list
+     * @return
+     */
+    public static <T>boolean isEmpty(List<T> list){
+
+        return list==null||list.size()==0;
+    }
+
+    /**
+     * 判断是否为空
+     * @param list
+     * @return
+     */
+    public static <T>boolean isNotEmpty(List<T> list){
+
+        return !isEmpty(list);
+    }
 }
